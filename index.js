250cbf9a00eb9a252221ba301ffdf22c1a5cd489
Damian <rascada@users.noreply.github.com>
2015-12-31 02:01:57 +0100

    little optimization

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 export default class ArrowChanger{
   constructor($core) {
     document.onkeydown = _=> {
-      switch (_.which) {
-        case 37:
-          $core.modules.slides.change(-1);
-          break;
-        case 39:
-          $core.modules.slides.change(1);
-          break;
+      if (_.which == 37 || _.which == 39) {
+        switch (_.which) {
+          case 37:
+            $core.modules.slides.change(-1);
+            break;
+          case 39:
+            $core.modules.slides.change(1);
+            break;
+        }
+  
+        if ($core.modules.autoSlider) 
+          $core.modules.autoSlider.restart(5000);
       }
-      if ((_.which == 37 || _.which == 39) && $core.modules.autoSlider) $core.modules.autoSlider.restart(5000);
     };
   }
 }
