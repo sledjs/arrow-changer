45159e05727f3d025635d302616740971db8d1d5
rascada <carftjr@gmail.com>
2016-02-04 23:12:49 +0100

    remove auto slider restarting

--- index.js
+++ index.js
@@ -2,22 +2,15 @@ export default class ArrowChanger{
   constructor($core) {
     let slides = $core.getModule('slides', '_');
 
-    document.onkeydown = _=> {
-      if (_.which == 37 || _.which == 39) {
-        switch (_.which) {
-          case 37:
-            slides.change(-1);
-            break;
-          case 39:
-            slides.change(1);
-            break;
-        }
-
-        $core.module('auto-slider')
-          .then(module =>
-            module._.restart(5000)
-          ).catch(_ => {});
+    document.addEventListener('keydown', key => {
+      switch (key.which) {
+        case 37:
+          slides.change(-1);
+          break;
+        case 39:
+          slides.change(1);
+          break;
       }
-    };
+    });
   }
 }
